329aff76000062ab7a0d5ef02f9996c59a7805e5
Luis Armando Perez Marin <lperez@i-t-m.com>
2015-12-03 13:08:53 -0600

    Creando menu y funciones de prueba para el menu, utilizando while loop

--- drp_ovirt.py
+++ drp_ovirt.py
@@ -1,5 +1,18 @@
 from __future__ import print_function
-from functions_ovirt import connect, status_one_host, read_config
+
+from functions_ovirt import connect, status_one_host, read_config, clear, menu
+
+
+def status():
+    clear()
+    print("status")
+    raw_input("Press Enter to continue...")
+
+
+def iniciar():
+    clear()
+    print("iniciar")
+    raw_input("Press Enter to continue...")
 
 
 def main():
@@ -10,15 +23,28 @@ def main():
     url_manager = 'https://' + manager
     hosts = config["Hosts"]
 
+    '''
     print(username)
     print(password)
     print(manager)
     print(url_manager)
-    print(hosts["local"])
-    print(hosts["remote"])
+    print(platform.system())'''
 
     api = connect(manager_url=url_manager, manager_password=password, manager_username=username)
-    print(status_one_host(api, hosts["remote"]["host1"]))
+
+    while True:
+        clear()
+        menu()
+        option = raw_input("Choice: ")
+
+        if option == "3":
+            break
+        elif option == "1":
+            status()
+        elif option == "2":
+            iniciar()
+        else:
+            print("no existe la opcion")
 
 
 if __name__ == '__main__':
