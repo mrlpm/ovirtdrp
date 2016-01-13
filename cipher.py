195a41e87070b20678e77205e9bafe5454a90fcd
Luis Perez <luis.luimarin@gmail.com>
2016-01-13 16:43:04 -0600

    cambiando la implementacion de cipher.py

--- cipher.py
+++ cipher.py
@@ -1,84 +1,37 @@
 from __future__ import print_function
 import random
-from optparse import OptionParser
+import string
+import base64
 
-import optparse
-
-
-def encrypt(k, verbose):
-    plaintext = raw_input('Enter plain text message: ')
-    cipher = ''
-    for each in plaintext:
-        c = (ord(each) + k) % 126
-        if c < 32:
-            c += 31
-        if verbose:
-            print("char is: %s ordchar %s Cchar %s ordCchar %s" % (each, ord(each), c, chr(c)))
-        cipher += chr(c)
-
-    print('Your encrypted message is: ' + cipher)
+punctuation = list(string.punctuation)
+alphabet = list(string.ascii_letters)
+chars = alphabet + punctuation
 
 
-def decrypt(k, verbose):
-    cipher = raw_input('Enter encrypted message: ')
-    plaintext = ''
-    for each in cipher:
-        p = (ord(each) - k) % 126
-
-        if p < 32:
-            p += 95
-        if verbose:
-            print("Cchar is: %s ordCchar %s Plain %s ord_plain %s " % (each, ord(each), p, chr(p)))
-        plaintext += chr(p)
-        plaintext.encode("utf-8")
-
-    print('Your plain text message is: ' + plaintext)
+def gen_key(array):
+    length = random.randint(4, 7)
+    master_key = random.sample(array, length)
+    key_str = ''.join(map(str, master_key))
+    return key_str.encode("hex")
 
 
-def gen_key():
-    master_key = random.randint(99, 1000)
-    while master_key % 126 == 0:
-        master_key = random.randint(99, 1000)
-    return master_key
+def encrypt(key_text, password_clear):
+    password_b64 = base64.b64encode(password_clear)
+    master_password = key_text + "@" + password_b64
+    return base64.b64encode(master_password)
 
 
-def main():
-    parser = OptionParser()
-    parser = optparse.OptionParser("usage: %prog [options] <string> or -h for help")
-    parser.add_option("-d", "--decrypt",
-                      action="store_true",
-                      dest="cstring",
-                      default=False,
-                      help="decrypt a previous string cipher with this script")
-    parser.add_option("-e", "--encrypt",
-                      action="store_true",
-                      dest="plain_string",
-                      default=False,
-                      help="encrypt a previous string cipher with this script")
-    parser.add_option("-v", "--verbose",
-                      action="store_true",
-                      dest="verbose",
-                      default=False,
-                      help="more verbose process")
-
-    (options, args) = parser.parse_args()
-
-    if not options.cstring:
-        if not options.plain_string:
-            if not options.verbose:
-                print("invalid number of argument")
-            elif options.verbose:
-                print("alone verbose options is not valid")
-
-    if options.plain_string:
-        master_key = gen_key()
-        print("Your Key is: %s" % master_key)
-        print("store in a safety place is not recoverable")
-        encrypt(master_key, options.verbose)
-    if options.cstring:
-        key = input("Enter your Key: ")
-        decrypt(key,options.verbose)
+def decrypt(cipher_password):
+    not_b64 = base64.b64decode(cipher_password)
+    key_encode, pass_encode = not_b64.split('@')
+    password_clear_text = base64.b64decode(pass_encode)
+    return password_clear_text
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    key = gen_key(chars)
+    password = raw_input("Password: ")
+    cipher_text = encrypt(key, password)
+
+    print("encrypted key: %s " % cipher_text)
+    print("decypted: %s " % decrypt(cipher_text))
