690b3f18c88faf1449ab7f6e90c463efb0773cfa
Luis Perez <luis.luimarin@gmail.com>
2016-01-14 14:50:44 -0600

    cambios a la configuracion y archivo para test de base de datos

--- test_db.py
+++ test_db.py
@@ -18,15 +18,24 @@ import itertools
 
 lun_local = luns['lunIDA']
 lun_remote = luns['lunIDB']
-for lunsa, lunsb in itertools.izip(lun_local, lun_remote):
-    print("changing %s by %s " % (lunsa, lunsb))
-    lunx = db.storage_server_connections.filter_by(iqn=lunsa).one()
-    lunx.iqn = lunsb
-    db.commit()
-    print("port: %s iqn: %s" % (lunx.port, lunx.iqn))
+portals_local = portals['iscsiportalA']
+portals_remote = portals['iscsiportalB']
+i=0
+#for lunsa, lunsb in itertools.izip(lun_local, lun_remote):
+    #print("changing %s by %s " % (lunsa, lunsb))
+    #print(db.storage_server_connections.filter_by(iqn=lunsa))
+    #lunx = db.storage_server_connections.filter_by(iqn=lunsa).all()
+    #i += 1
+    #print(i)
+    #print(lunx)
 
-print("#######################")
+for portala, portalb in itertools.izip(portals_local, portals_remote):
+    #print(db.storage_server_connections.filter_by(connection=portala))
+    portalx = db.storage_server_connections.filter_by(connection=portala).all()
+    #print(portalx)
+    print("#######################")
 
-print(lun_local[0])
-
+print(db.storage_server_connections.filter_by(connection='192.168.113.254'))
+dos = db.storage_server_connections.filter_by(connection='192.168.113.254').one()
+print(dos)
 #####
