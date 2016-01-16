a88e1424d380cd4a8efa43580cedb4ee0f362e3b
Luis Perez <luis.luimarin@gmail.com>
2016-01-16 08:25:40 -0600

    refactoring script bash DRP.sh

--- DRP.sh
+++ DRP.sh
@@ -6,33 +6,31 @@ CONFFILE=config.yml
 CONFLIST=${CONFDIR}/*Fail*
 
 function set_config() {
-ln -sf $1 ${CONFDIR}/${CONFFILE}
+    ln -sf $1 ${CONFDIR}/${CONFFILE}
 }
 
 function config_menu() {
-CONFs=""
+    CONFs=""
 
-NUM=0
-for CONF in ${CONFLIST}
-do
-NUM=$[NUM+1]
-CONFs="$CONFs $CONF $(basename -s .yml $CONF) "
-done
-OPTION=$(whiptail --title "Menu DRP - Ministerio Hacienda" --menu "Choose your option" 15 60 4 \
-$CONFs 3>&1 1>&2 2>&3)
+    NUM=0
+    for CONF in ${CONFLIST}
+    do
+        NUM=$[NUM+1]
+        CONFs="$CONFs $CONF $(basename -s .yml $CONF) "
+    done
+    OPTION=$(whiptail --title "Menu DRP - Ministerio Hacienda" --menu "Choose your option" 15 60 4 \
+    $CONFs 3>&1 1>&2 2>&3)
 
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-clear
-set_config $OPTION
-echo "Your using $(basename -s .yml $CONF) "
-echo ""
-~/ovirtdrp/drp_ovirt.py
-
-else
-exit 1
-fi
-
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+        clear
+        set_config $OPTION
+        echo "Your using $(basename -s .yml $CONF) "
+        echo ""
+        ~/ovirtdrp/drp_ovirt.py
+    else
+        exit 1
+    fi
 }
 
 config_menu
